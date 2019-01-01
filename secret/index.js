7ee387b755db1589c49aa2422991736b3c050869
Mohammed Faizuddin <mdfaizuddin15@gmail.com>
2019-01-01 23:01:42 +0530

    tests passing for containsLetters

--- secret/index.js
+++ secret/index.js
@@ -1,2 +1,13 @@
-const containsLetters = (needle, haystack) => {};
+const { characterCount, characterHashMap } = require("./../utils");
+
+const containsLetters = (needle, haystack) => {
+  const needleHashMap = characterHashMap(needle);
+  const lettersInNeedle = Object.keys(needleHashMap);
+  if (lettersInNeedle.length === 0) {
+    return false;
+  }
+  return lettersInNeedle.every(
+    letter => characterCount(letter, haystack) >= needleHashMap[letter]
+  );
+};
 exports.containsLetters = containsLetters;
