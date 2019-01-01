5367ac495bf3c8ad8b4c4c841cb1bb4e112b8d72
Mohammed Faizuddin <mdfaizuddin15@gmail.com>
2019-01-01 23:39:23 +0530

    tests passing for processArg

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const parseArg = (args, allowed) => {
-  
+  const sliced = args.slice(2);
+  const firstArg = sliced[0];
+  if (allowed.indexOf(firstArg) === -1) {
+    console.log(`only allowed ${allowed.join(", ")}`);
+    process.exit(0);
+  }
+  return firstArg;
 };
 exports.parseArg = parseArg;
