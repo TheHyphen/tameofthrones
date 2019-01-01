86bc6d9bd7187dd16e7c89d4c698c8f86ff076d3
Mohammed Faizuddin <mdfaizuddin15@gmail.com>
2019-01-01 22:08:48 +0530

    tests pass for characterCount

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,18 @@
+const isStringLike = string => {
+  const type = typeof string;
+  return type === "string" || type === "number";
+};
+
 const characterCount = (character, string) => {
+  if (!isStringLike(string) || !isStringLike(character)) {
+    return 0;
+  }
+  if (string.toString().length === 0 || character.toString().length === 0) {
+    return 0;
+  }
+  const matches = string
+    .toString()
+    .match(new RegExp(character.toString(), "g"));
+  return matches && matches.length ? matches.length : 0;
 };
 exports.characterCount = characterCount;
