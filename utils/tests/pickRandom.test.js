b88d68d805e517b9bd9e073a4937fbf947b1a62e
Mohammed Faizuddin <mdfaizuddin15@gmail.com>
2019-01-03 22:12:38 +0530

    adds test for overflow in pickRandom

--- utils/tests/pickRandom.test.js
+++ utils/tests/pickRandom.test.js
@@ -9,3 +9,9 @@ test("pickRandom picks n random elements from array", t => {
     t.true(input.indexOf(o) !== -1);
   });
 });
+
+test("pickRandom should handle outOfBounds", t => {
+  const input = ["a", "b", "c", "d"];
+  const output = pickRandom(input, 6);
+  t.is(output.length, 4);
+});
