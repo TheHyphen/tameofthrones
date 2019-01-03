2b9555c1cd5b188ba6d56b33ae6a569d0b7160e4
Mohammed Faizuddin <mdfaizuddin15@gmail.com>
2019-01-03 22:35:32 +0530

    adds getBallotMessages tests and placeholder

---
+++ ballot/tests/getBallotMessages.test.js
@@ -0,0 +1,24 @@
+import test from "ava";
+const { getBallotMessages } = require("./../index");
+
+test("getBallotMessages should return flat messages list", t => {
+  const participants = ["air", "water"];
+  const allies = ["ice", "fire"];
+  const messages = ["a", "b", "c", "d"];
+
+  const output = getBallotMessages(participants, allies, messages);
+
+  t.true(
+    // output is flat
+    output.every(o => typeof o === "object" && !Array.isArray(o))
+  );
+
+  t.true(
+    output.every(
+      o =>
+        typeof o.ally === "string" &&
+        typeof o.participant === "string" &&
+        typeof o.message === "string"
+    )
+  );
+});
