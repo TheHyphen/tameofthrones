326089f1a6c51483595e1c357715cf209f71aebd
Mohammed Faizuddin <mdfaizuddin15@gmail.com>
2019-01-03 22:37:14 +0530

    tests passing for getPollResults

--- ballot/index.js
+++ ballot/index.js
@@ -32,5 +32,29 @@ const getBallotMessages = (participants, allies, messages) => {
 };
 exports.getBallotMessages = getBallotMessages;
 
-const getPollResults = (participants, messages, emblemsHash) => {};
+const getPollResults = (participants, messages, emblemsHash) => {
+  const scoresHash = {};
+  const alliesHash = {};
+  const takenAllies = [];
+  let highScore = 0;
+
+  participants.forEach(participant => {
+    scoresHash[participant] = 0;
+    alliesHash[participant] = [];
+  });
+
+  messages.forEach(({ message, ally, participant }) => {
+    if (takenAllies.indexOf(ally) === -1) {
+      if (containsLetters(emblemsHash[ally], message)) {
+        scoresHash[participant] = scoresHash[participant] + 1;
+        if (highScore < scoresHash[participant]) {
+          highScore = scoresHash[participant];
+        }
+        alliesHash[participant].push(ally);
+        takenAllies.push(ally);
+      }
+    }
+  });
+  return { scoresHash, alliesHash, highScore };
+};
 exports.getPollResults = getPollResults;
