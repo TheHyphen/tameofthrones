088995698c8606ad5203db7b98204a48649ac0e7
Mohammed Faizuddin <mdfaizuddin15@gmail.com>
2019-01-04 18:56:50 +0530

    improved getUserInput handling

--- goldencrown/getUserInput.js
+++ goldencrown/getUserInput.js
@@ -1,58 +1,38 @@
 const readlineSync = require("readline-sync");
-const {
-  isStringLike,
-  splitBy,
-  isKingdom,
-  trim,
-  toLowerCase
-} = require("../utils");
-
-function validate(input, kingdoms) {
-  if (!isStringLike(input)) {
-    return false;
-  }
-  const parts = input.toString().split(",");
-  if (parts.length < 2) {
-    return false;
-  }
-  const kingdom = parts[0];
-  if (isKingdom(kingdom, kingdoms)) {
-    return true;
-  }
-  return false;
-}
+const { isStringLike, isKingdom, trim } = require("../utils");
 
 function getUserInput(kingdoms) {
   if (!Array.isArray(kingdoms) || !kingdoms.every(isStringLike)) {
     throw new Error("kingdoms must be an array of strings");
   }
   console.log("Enter messages for King Shah:");
-  const inputs = [];
+  const inputs = {};
   while (true) {
     const input = readlineSync.question("");
-    if (input === "") {
+    if (input === "" && Object.keys(inputs).length !== 0) {
       break;
     }
-    if (validate(input, kingdoms)) {
-      inputs.push(toLowerCase(trim(input)));
-      if (inputs.length >= kingdoms.length) {
+    const { kingdom, message } = parseUserInput(input);
+    if (isKingdom(kingdom, kingdoms) && isStringLike(message)) {
+      inputs[kingdom] = message;
+      if (Object.keys(inputs).length >= kingdoms.length) {
         break;
       }
     } else {
-      console.log(`Kingdom not allowed. Try again.`);
-      console.log(`Allowed: ${kingdoms.join(", ")}`);
+      console.log(`Invalid input, please try again.`);
+      console.log(`Allowed Kingdoms: ${kingdoms.join(", ")}`);
+      console.log(`Format: <kindom>, <message for the kingdom>`);
     }
   }
-  return getKindomMessagesHash(inputs);
+  return inputs;
 }
 exports.getUserInput = getUserInput;
 
-function getKindomMessagesHash(inputs) {
-  return inputs.map(splitBy(",")).reduce((acc, input) => {
-    return {
-      ...acc,
-      [input[0]]: input[1]
-    };
-  }, {});
+function parseUserInput(input) {
+  const parts = input.split(",").map(trim);
+  return {
+    kingdom: parts[0],
+    message: parts[1]
+  };
 }
-exports.getHashFromInputs = getKindomMessagesHash;
+exports.parseUserInput = parseUserInput;
